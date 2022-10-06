bdb539e998b5164a8422b95bbae952a22d040c3b
intartaa1 <intartaa1@newpaltz.edu>
2022-10-06 16:37:14 -0400

    Vue/Bulma website changes
    Nav, App, & AboutView pages

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,19 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 import NavVue from './components/Nav.vue'
+
+
 </script>
 
 <template>
   <header>
     <NavVue />
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
+
 <div class="container">
   <RouterView />
 </div>
+
 </template>
 
 <style scoped>
--- client/src/main.ts
+++ client/src/main.ts
@@ -4,8 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import 'bulma'
-//import "./assets/main.css";
+import "./assets/main.scss";
 
 const app = createApp(App);
 
