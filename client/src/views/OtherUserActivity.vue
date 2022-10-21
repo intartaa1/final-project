0cc26b4b9ab618baf5ec599f577ac25edd5c64e0
intartaa1 <intartaa1@newpaltz.edu>
2022-10-20 22:42:26 -0400

    separate pages for a user to view his/her own activiies and a page to view his/her friends

---
+++ client/src/views/OtherUserActivity.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1 class="title">Your Friends' Activities</h1>
+        <p>Also check out how your friends have been keeping busy!</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ client/src/views/UserActivity.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1 class="title">My Activity</h1>
+        <p>Check out the fun and cool ways you've been keeping active lately!</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
