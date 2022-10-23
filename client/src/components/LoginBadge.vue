a8cb75dfc451774c5fbcdd91b0433b22d9aaaa36
intartaa1 <intartaa1@newpaltz.edu>
2022-10-23 16:53:19 -0400

    login badge page corrections
    new template to login into any user

--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -1,33 +1,35 @@
 <script setup lang="ts">
     import session, { login, logout } from '../stores/session'
 </script>
-
 <template>
-    <div class="buttons" v-if="session.user == null">
-        <a class="button is-primary">
-            <strong>Sign up</strong>
-        </a>
-        <div class="userRow">
-            <label for="user">Users</label>
-            <select name="userType" id="user">
-                <option value="user1">Antonio Intartaglia</option>
-                <option value="user2">Moshe Plotkin</option>
-                <option value="user3">Lenore Perez</option>
-            </select>
+    <div class="dropdown is-hoverable">
+        <div class="dropdown-trigger">
+            <button class="button" aria-hasgroup="true" aria-controls="dropdown-menu">
+                <span>Login</span>
+                <span class="icon is-small">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+            </button>
         </div>
-        <a class="button is-light" @click="login('Antonio', 'Intartaglia')">
-            Log in
-        </a>
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+            <div class="dropdown-content">
+                <a href="#" class="dropdown-item">
+                    Antonio Intartaglia
+                </a>
+                <a class="dropdown-item">
+                    Moshe Plotkin
+                </a>
+                <a href="#" class="dropdown-item">
+                    Lenore Perez
+                </a>
+                <hr class="dropdown-divider">
+                <a href="#" class="dropdown-item">
+                    Other User
+                </a>
+            </div>
+        </div>
     </div>
-    <div v-else>
-        Welcome {{session.user.firstName}} {{session.user.lastName}}
-        (<a @click="logout()">
-            Log out
-        </a>)
-    </div>
-
 </template>
-
 
 <style scoped>
 </style>
