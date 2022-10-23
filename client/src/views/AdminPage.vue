e3ccb9544a979b55c2c2eed2731b34b3008f010c
intartaa1 <intartaa1@newpaltz.edu>
2022-10-23 14:43:59 -0400

    admin page creation
    rows and information added to the admin page

---
+++ client/src/views/AdminPage.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="columns">
+        <div class="column">
+            First Name
+        </div>
+        <div class="column">
+            Last Name
+        </div>
+        <div class="column">
+            Emails
+        </div>
+        <div class="column">
+            Handle
+        </div>
+        <div class="column">
+            Is Admin
+        </div>
+    </div>
+    <div class="columns">
+        <div class="column">
+            Antonio
+        </div>
+        <div class="column">
+            Intartaglia
+        </div>
+        <div class="column">
+            intartaa1@newpaltz.edu
+        </div>
+        <div class="column">
+            Intartaglia, Antonio
+        </div>
+        <div class="column">
+            True
+        </div>
+    </div>
+    <div class="columns">
+        <div class="column">
+            Moshe
+        </div>
+        <div class="column">
+            Plotkin
+        </div>
+        <div class="column">
+            plotkinm1@newpaltz.edu
+        </div>
+        <div class="column">
+            Plotkin, Moshe
+        </div>
+        <div class="column">
+            True
+        </div>
+    </div>
+    <div class="columns">
+        <div class="column">
+            Lenore
+        </div>
+        <div class="column">
+            Perez
+        </div>
+        <div class="column">
+            lenoreperez@gmail.com
+        </div>
+        <div class="column">
+            Perez, Lenore
+        </div>
+        <div class="column">
+            False
+        </div>
+    </div>
+
+</template>
