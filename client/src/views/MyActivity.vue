dc8be367c039c459d0fefdbd8e5bc0f60ef56f1d
intartaa1 <intartaa1@newpaltz.edu>
2022-10-23 16:18:07 -0400

    creation of my activity page, edits in other user activities page, and pushing images for website

---
+++ client/src/views/MyActivity.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h1 class="title">My Activities</h1>
+        <h2>Check out the cool and fun ways you've been keeping yourself active!</h2>
+    </div>
+</template>
+
+<style scoped>
+</style>
