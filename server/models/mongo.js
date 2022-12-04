d83f1b371d19569106f605e6392d7d440953d719
intartaa1 <intartaa1@newpaltz.edu>
2022-12-04 17:39:13 -0500

    index and mongo file creations

---
+++ server/models/mongo.js
@@ -0,0 +1,9 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://intartaa1:<password>@cluster0.4xps8lk.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
