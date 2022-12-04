e175a5893e294aa47f572aa74f199e5ada731b23
intartaa1 <intartaa1@newpaltz.edu>
2022-12-04 18:14:36 -0500

    mongo & login badge fixes

--- server/models/mongo.js
+++ server/models/mongo.js
@@ -1,9 +1,11 @@
 
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://intartaa1:<password>@cluster0.4xps8lk.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const uri = process.env.MONGO_URI ?? "";
+const options = { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }
+
+const client = new MongoClient(uri, options);
+
+module.exports = {
+    connect: ()=> client.connect() 
+}
