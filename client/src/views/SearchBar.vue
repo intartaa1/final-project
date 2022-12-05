5ffa5a1779fff4472aa8c246c2090e4bb77487be
intartaa1 <intartaa1@newpaltz.edu>
2022-12-04 20:55:44 -0500

    bringing back myFetch & edits to search bar

--- client/src/views/SearchBar.vue
+++ client/src/views/SearchBar.vue
@@ -1,6 +1,27 @@
 <script setup lang="ts">
     import { isLoading } from "@/stores/session";
-    import { ref } from "vue";
-    import { useRoute } from "vue-router";
-    const route = useRoute();
+    import { computed, reactive, ref, watch } from "vue";
+    import { RouterLink } from "vue-router";
+    import { getUsers, type User } from "../stores/products";
+
+    const users = reactive([] as User[]);
+    getUsers().then( x => users.push(...x.users));
+
+    const search = ref("");
 </script>
+
+<template>
+    <div>
+        <div class="control">
+            <input class="input" type="text" placeholder="Search" v-model="search" />
+        </div>
+
+        <div class="users">
+            <RouterLink v-for="user in users" :key="user.name"
+                        class="user" :class="{ 'is-disabled': isLoading }"
+                        :to="'/user/${user.name}'"
+                        v-show="user.title.toLowerCase().includes(search.toLowerCase())">
+                    </RouterLink>
+        </div>
+    </div>
+</template>
