7f57c21fe82e3ceac25abfe77f370d7e4fa25c4d
intartaa1 <intartaa1@newpaltz.edu>
2022-12-05 17:24:25 -0500

    putting in images for activity pages, importing them from vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  resolve: {
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: 'src/views/thatActivity.vue'
+    },
+  }
+  ,resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     }
