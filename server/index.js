21df7580c35d579631d412f600ffff8ef345c199
intartaa1 <intartaa1@newpaltz.edu>
2022-12-14 13:32:40 -0500

    adding postController lines back

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-//const postsController = require('./controllers/posts');
+const postsController = require('./controllers/posts');
 
 const hostname = '12.0.0.1';
 const port = process.env.PORT || 3003;
 const app = express();
 
 app.use(express.static('no-framework'));
-//app.use('/v1/posts/', postsController);
+app.use('/v1/posts/', postsController);
 app.listen(port, ()=>{
     console.log('Server running at http://${hostname}:${port}/');
 })
