6c79e395cbef78d0cd99e6085b6e7cd8024854f1
intartaa1 <intartaa1@newpaltz.edu>
2022-12-14 13:52:27 -0500

    index.js add-ins

--- server/index.js
+++ server/index.js
@@ -1,12 +1,46 @@
 const express = require('express')
-const postsController = require('./controllers/posts');
+const app = express()
 
-const hostname = '12.0.0.1';
+const usersController = require('./controllers/users');
+
+const hostname = '127.0.0.1';
 const port = process.env.PORT || 3003;
-const app = express();
 
-app.use(express.static('no-framework'));
-app.use('/v1/posts/', postsController);
-app.listen(port, ()=>{
-    console.log('Server running at http://${hostname}:${port}/');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+
 })
+
+app.use('/', express.static('./client/dist'));
+
+app.use(express.json());
+
+app
+.get('/', (req, res) => {
+    res.status(200).send('Happy Sweet New Year');
+})
+.get('/error', (req, res) => {
+    sss.PORT();
+})
+.use('/api/v1/users', usersController)
+
+app.get('*', (req, res) => {
+    
+    res.sendFile('index.html', {root: './client/dist'});
+})
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status( err.httpCode ?? 500).send({
+        message: err.message ?? 'Something went wrong',
+        status: err.httpCode ?? 500
+    });
+
+})
+
+app.listen(port, ()=> {
+    console.log('Server running at http://${hostname}:${port}/');
+});
