1a7bdcad71b8ea48daa7c639b9813cd13b3fdd7a
intartaa1 <intartaa1@newpaltz.edu>
2022-12-14 12:23:49 -0500

    adding an email and a password after each dropdown item

--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -11,13 +11,13 @@
         </div>
         <div class="dropdown-menu" id="dropdown-menu" role="menu">
             <div class="dropdown-content">
-                <a class="dropdown-item" @click="login('Antonio', 'Intartaglia')">
+                <a class="dropdown-item" @click="login('Antonio', 'Intartaglia', 'intartaa1@newpaltz.edu', 'Password')">
                     Antonio Intartaglia
                 </a>
-                <a class="dropdown-item" @click="login('Moshe', 'Plotkin')">
+                <a class="dropdown-item" @click="login('Moshe', 'Plotkin', 'plotkinm1@gmail.com', 'Password')">
                     Moshe Plotkin
                 </a>
-                <a class="dropdown-item" @click="login('Lenore', 'Perez')">
+                <a class="dropdown-item" @click="login('Lenore', 'Perez', 'lenoreperez@gmail.com', 'Password')">
                     Lenore Perez
                 </a>
                 </div>
