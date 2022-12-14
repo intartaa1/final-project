54b4f83e472782d30961eebcfe85b075eb8decb1
intartaa1 <intartaa1@newpaltz.edu>
2022-12-14 12:58:16 -0500

    index.js correction

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const postsController = require('.controllers/posts');
+const postsController = require('./controllers/posts');
 
 const hostname = '12.0.0.1';
 const port = process.env.PORT || 3003;
@@ -8,5 +8,5 @@ const app = express();
 app.use(express.static('no-framework'));
 app.use('/v1/posts/', postsController);
 app.listen(port, ()=>{
-    console.log('Server running at http://${hostname}:${port}/')
+    console.log('Server running at http://${hostname}:${port}/');
 })
