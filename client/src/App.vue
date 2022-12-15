1da96c2ba2f89d11608917c79f093bf08f5137f7
intartaa1 <intartaa1@newpaltz.edu>
2022-12-15 14:09:27 -0500

    updates to the app view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
     import { RouterView } from 'vue-router'
     import NavVue from './components/Nav.vue';
-    
+    import SearchAutocomplete from './components/SearchAutocomplete.vue';
+
+    export default {
+      name: 'App'
+      ,components: {
+        SearchAutocomplete
+      }
+    };
 </script>
 
 <template>
-    
   <header>
     <NavVue />
   </header>
+
+  <div id="app">
+    <SearchAutocomplete
+     :items="[
+      'Bike Riding',
+      'Deadlifting',
+      'Fishing',
+      'Jogging',
+      'Rollerskating',
+      'Skatebaording'
+     ]"
+     />
+  </div>
+
 
   <div class="container">
     <RouterView />
