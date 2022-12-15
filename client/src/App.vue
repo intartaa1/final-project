ff895d0e474e20f063ac4a64e71b7552e2628875
intartaa1 <intartaa1@newpaltz.edu>
2022-12-15 16:16:00 -0500

    playing around with search componenet

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,13 +2,6 @@
     import { RouterView } from 'vue-router'
     import NavVue from './components/Nav.vue';
     import SearchAutocomplete from './components/SearchAutocomplete.vue';
-
-    export default {
-      name: 'App'
-      ,components: {
-        SearchAutocomplete
-      }
-    };
 </script>
 
 <template>
